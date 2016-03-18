d298cc3a976e7d49ebb820792b2a9708c6368649
Mleon195 <mleon@mail.ccsf.edu>
2016-03-17 18:41:12 -0700

    update

--- change/base.js
+++ change/base.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
   $("#time").text( Date.now() );
 
   $("input.add").on("change", function handleChange(event){
-    var left = $("input#left").val();
-    var right = $("input#right").val();
-    var leftVal = parseInt(left) || 0;
-    var rightVal = parseInt(right) || 0;
-    var total = leftVal + rightVal;
-    $("#total").val(total);
-  })
 
-})
+   var left= parseInt($("input#left").val());
+   var right = parseInt($("input#right").val());
+   var total = left + right;
+   $("#total").val(total).val();
+
+ });
+
+});
